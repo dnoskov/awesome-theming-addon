8995a919e95bc1a050391004fac8058e7cc3842c
Dmitriy Noskov <nda85@ya.ru>
2011-06-10 22:59:23 +0400

    Прибрался

--- lib/configs.lua
+++ lib/configs.lua
@@ -1,37 +1,63 @@
 require("awful.util")
 local utils = require "utils"
 
-local configs = {}
-
-local gtktest = {
-   data = {
-      ["gtk-theme-name"]        = '"Termlike"',
-      ["gtk_color_scheme"] = {
-	 ["fg_color"]          = "#ff0000",
-	 ["bg_color"]          = "#ff0000",
-	 ["base_color"]        = "#ff0000",
-	 ["text_color"]        = "#ff0000",
-	 ["selected_bg_color"] = "#ff0000",
-	 ["selected_fg_color"] = "#ff0000",
-	 ["tooltip_fg_color"]  = "#ff0000",
-	 ["tooltip_bg_color"]  = "#ff0000",
+local configs = {
+   tests = {
+      gtk = {
+	 data = {
+	    ["gtk-theme-name"]        = '"Termlike"',
+	    ["gtk_color_scheme"] = {
+	       ["fg_color"]          = "#ff0000",
+	       ["bg_color"]          = "#ff0000",
+	       ["base_color"]        = "#ff0000",
+	       ["text_color"]        = "#ff0000",
+	       ["selected_bg_color"] = "#ff0000",
+	       ["selected_fg_color"] = "#ff0000",
+	       ["tooltip_fg_color"]  = "#ff0000",
+	       ["tooltip_bg_color"]  = "#ff0000",
+	    },
+	    ["gtk-icon-theme-name"]   = '"ACYL_Icon_Theme_0.9.3"',
+	    ["gtk-font-name"]         = '"Sans 8"',
+	    ["gtk-cursor-theme-name"] = '"OpenZone_Black_Slim"',
+	    ["gtk-cursor-theme-size"] = 0,
+	    ["gtk-toolbar-style"]     = "GTK_TOOLBAR_BOTH_HORIZ",
+	    ["gtk-toolbar-icon-size"] = "GTK_ICON_SIZE_LARGE_TOOLBAR",
+	    ["gtk-button-images"]     = "1",
+	    ["gtk-menu-images"]       = "1",
+	    ["gtk-enable-event-sounds"] = "1",
+	    ["gtk-enable-input-feedback-sounds"] = "1",
+	    ["gtk-xft-antialias"]     = "1",
+	    ["gtk-xft-hinting"]       = "1",
+	    ["gtk-xft-hintstyle"]     = '"hintfull"',
+	    ["gtk-xft-rgba"]          = '"rgb"',
+	 }
       },
-      ["gtk-icon-theme-name"]   = '"ACYL_Icon_Theme_0.9.3"',
-      ["gtk-font-name"]         = '"Sans 8"',
-      ["gtk-cursor-theme-name"] = '"OpenZone_Black_Slim"',
-      ["gtk-cursor-theme-size"] = 0,
-      ["gtk-toolbar-style"]     = "GTK_TOOLBAR_BOTH_HORIZ",
-      ["gtk-toolbar-icon-size"] = "GTK_ICON_SIZE_LARGE_TOOLBAR",
-      ["gtk-button-images"]     = "1",
-      ["gtk-menu-images"]       = "1",
-      ["gtk-enable-event-sounds"] = "1",
-      ["gtk-enable-input-feedback-sounds"] = "1",
-      ["gtk-xft-antialias"]     = "1",
-      ["gtk-xft-hinting"]       = "1",
-      ["gtk-xft-hintstyle"]     = '"hintfull"',
-      ["gtk-xft-rgba"]          = '"rgb"',
+      xcolors = {
+	 files = { os.getenv("HOME") .. "/.Xcolors" },
+	 data = {
+	    ["foreground"] = "#dbe1ee",
+	    ["background"] = "#1b1e21",
+	    ["color0"] = "#1b1e21",
+	    ["color8"] = "#16141e",
+	    ["color1"] = "#b04734",
+	    ["color9"] = "#c9756a",
+	    ["color2"] = "#234c19",
+	    ["color10"] = "#5ea02f",
+	    ["color3"] = "#b09a34",
+	    ["color11"] = "#c9b56a",
+	    ["color4"] = "#1e445e",
+	    ["color12"] = "#6caabf",
+	    ["color5"] = "#501b39",
+	    ["color13"] = "#a6409d",
+	    ["color6"] = "#637287",
+	    ["color14"] = "#b1bacb",
+	    ["color7"] = "#dbe1ee",
+	    ["color15"] = "#ffffff",
+	 }
+      }
    }
 }
+
 
 function configs.createGTKrcString (cfg)
    cfgstr = ""
@@ -50,29 +76,6 @@ function configs.createGTKrcString (cfg)
    cfg.strings = { cfgstr .. scheme }
 end
 
-local testxcolors = {
-   files = { os.getenv("HOME") .. "/.Xcolors" },
-   data = {
-      ["foreground"] = "#dbe1ee",
-      ["background"] = "#1b1e21",
-      ["color0"] = "#1b1e21",
-      ["color8"] = "#16141e",
-      ["color1"] = "#b04734",
-      ["color9"] = "#c9756a",
-      ["color2"] = "#234c19",
-      ["color10"] = "#5ea02f",
-      ["color3"] = "#b09a34",
-      ["color11"] = "#c9b56a",
-      ["color4"] = "#1e445e",
-      ["color12"] = "#6caabf",
-      ["color5"] = "#501b39",
-      ["color13"] = "#a6409d",
-      ["color6"] = "#637287",
-      ["color14"] = "#b1bacb",
-      ["color7"] = "#dbe1ee",
-      ["color15"] = "#ffffff",
-   }
-}
 
 
 function configs.createXcolorsString (cfg)
@@ -83,14 +86,5 @@ function configs.createXcolorsString (cfg)
    cfg.strings = { cfgstr }
 end
 
-function configs.xcolorsApply (cfg)
-   for num, cfgfile in pairs(cfg.files) do
-      io.write("Attempting to update XRDB with " .. cfgfile .. " ... ")
-      cmd = "xrdb -merge " .. cfgfile
-      io.write("\nExecuting " .. cmd)
-      awful.util.spawn_with_shell(cmd)
-      io.write(" ... Done.")
-   end
-end
 
 return configs
