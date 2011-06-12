10da7b89ffe9f2ed61f79b930613956134e0d04a
Dmitriy Noskov <nda85@ya.ru>
2011-06-12 23:49:43 +0400

    Написана базовая функциональность. Недоработано (см. TODO)

--- lib/acyl.lua
+++ lib/acyl.lua
@@ -1,43 +1,90 @@
 require 'lfs'
 require 'pl'
-local acyl       = {
+require 'vardump'
+
+
+local acyl = {
    test = {
-      files = {},
-      path = { os.getenv("HOME") .. "/.icons/ACYL_Icon_Theme_0.9.3" },
+      icons = {
+
+      },
+      paths = { 
+	 source = os.getenv("HOME") .. "/.icons/ACYL_Icon_Theme_0.9.3",
+	 dest = "/home/dnoskov/.config/awesome/themes/medusa/icons",
+	 symlinkto = "/home/dnoskov/.icons/awesome-icon-theme"
+      },
       patterns = { ".+svg$", ".+png$"},
+      data = {
+	 ['all'] = {
+	    template = "/home/dnoskov/.config/awesome/themes/medusa/icons/one_color_flat.xml",
+	    color = "#00AAFF"
+	 },
+	 ['real_icons/actions/Close.svg'] = {
+	    template = "/home/dnoskov/.config/awesome/themes/medusa/icons/one_color_flat.xml",
+	    color = "#FF0000"
+	 }
+      }
    }
 }
 
--- находит в директории cfg.path все файлы (не симлинки), соответствующие
--- каждому шаблону cfg.patterns и записывает в таблицу cfg.files полученный
--- список файлов
-function acyl.appendFiles (cfg)
+function acyl.maintainIcons (cfg)
+   local function esc(str)
+      res = ""
+      for char in List.iter(str) do
+	 if string.match(char, "[%^%$%(%)%%%.%[%]%*%+%-%?]") then
+	    res = res .. "%" .. char
+	 else res = res .. char end
+      end
+      return res
+   end
+
+   -- Заполнение cfg.icons
    local function getlist (pth, ptn)
-      for filename in lfs.dir(pth) do
-	 if filename ~= "." and filename ~= ".." then
-	    local fpath = pth .. "/" .. filename
+      for sourcefn in lfs.dir(pth) do
+	 if sourcefn ~= "." and sourcefn ~= ".." then
+	    local fpath = pth .. "/" .. sourcefn
 	    local attr = lfs.symlinkattributes(fpath)
 	    assert (type(attr) == "table")
 	    if attr.mode == "directory" then
 	       getlist(fpath, ptn)
 	    elseif attr.mode == "file" then
-	       if string.match(filename, ptn) then
-		  table.insert(cfg.files, fpath)
+	       if string.match(sourcefn, ptn) then
+		  table.insert(cfg.icons, fpath)
 	       end
 	    end
 	 end
       end
    end
 
-   for ipath in List.iter(cfg.path) do
-      for patrn in List.iter(cfg.patterns) do
-	 getlist(ipath, patrn)
-      end
-   end   
-end
+   for pattrn in List.iter(cfg.patterns) do
+      getlist(cfg.paths.source, pattrn)
+   end
+   -- cfg.icons заполнена
 
-function acyl.resetFileList (cfg)
-   cfg.files = {}
+  
+
+   -- Создание структуры директорий
+   -- for icon in List.iter(cfg.icons) do
+   --    pth = string.gsub(string.gsub(icon, esc(cfg.paths.source), cfg.paths.dest), esc(path.basename(icon)), "")
+   --    if not path.exists(pth) then dir.makepath(pth) end
+   -- end
+
+   for srcicon in List.iter(cfg.icons) do
+      desticon = string.gsub(srcicon, cfg.paths.source, cfg.paths.dest)
+      iconRelativePath = string.gsub(srcicon, cfg.paths.source.."/scalable/", "")
+      for iconPath, iconData in pairs(cfg.data) do
+	 if iconpath == iconRelativePath then
+	    t = text.Template(utils.readfile(icondata.template))
+	    replstr = "<acyl-settings>\n"..t:substitute(icondata).."\n</acyl-settings>\n"
+	 else
+	    t = text.Template(utils.readfile(cfg.data['all'].template))
+	    replstr = "<acyl-settings>\n"..t:substitute(cfg.data['all']).."\n</acyl-settings>\n"
+	 end
+      end
+      srcstr = utils.readfile(srcicon)
+      deststr = string.gsub(srcstr, "<acyl%-settings>.*</acyl%-settings>", replstr)
+      utils.writefile(desticon, deststr)
+   end
 end
 
 return acyl
